a0668f4afded5d58eed55ad9ac2394d9f7a12e42
legzio <B00148761@mytudublin.ie>
2021-12-01 03:33:41 -0500

    new

---
+++ Symmetric_Ciphers/symmetry.py
@@ -0,0 +1,13 @@
+
+
+{"ciphertext":"2202d659074d1d125046c6779194fa46df1c418d432b78e4f346f13d3bf413144ee7d5f8bee4e2557eac60c2c7ae2b6916"}
+
+iv = 202d659074d1d125046c6779194fa46d
+
+plain = aaaaaaaaaaaaaaaa  (hex = 61616161616161616161616161616161)
+
+encrypt(plaintext,iv) :
+
+{"ciphertext":"194073682279360a"}
+
+key ^ iv = plain ^ cipher --> key = plain ^ cipher ^ iv = 61616161616161616161616161616161 ^ 
